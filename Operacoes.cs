ce70e24b2c698eddb02e947936a53366cc32528b
Sebastião Soares da Silva Filho <123883250+seb2301@users.noreply.github.com>
2024-04-25 21:59:12 -0300

    Add files via upload

---
+++ Operacoes.cs
@@ -0,0 +1,7 @@
+namespace Batata {
+    public class Operacoes {
+        public static int Somar(int numero1, int numero2) {
+            return numero1 + numero2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Batata {
+    public class Program {
+
+        public static int Somar(int numero1, int numero2) {
+        return numero1 + numero2;  
+    }
+
+        private static void Main(string[] args)
+        {
+
+            int num1, num2, resultado;
+
+            Console.WriteLine("Digite um número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A soma é: " + (num1 + num2));
+
+            resultado = Programex.Somar(num1, num2);
+
+            Console.WriteLine("A soma é: " + (resultado) + "saiudhsiaudhsaui");
+            Console.WriteLine($"A soma é: {resultado} saduishadihsaiduhsauidh");
+
+
+
+        }
+
+    }
+}
